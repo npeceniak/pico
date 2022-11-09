a8018070ac34e9a3d0d5e5f5a0bbd0d226f5a883
Nick Peceniak <npeceniak@magnite.com>
2022-11-08 21:09:22 -0700

    Final soldered fans

--- src/fansSoldered.py
+++ src/fansSoldered.py
@@ -6,14 +6,17 @@ onboard_led = machine.Pin(25, machine.Pin.OUT)
 button_0 = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 button_1 = machine.Pin(1, machine.Pin.IN, machine.Pin.PULL_UP)
 
+fan0 = machine.Pin(2, machine.Pin.OUT)
+fan1 = machine.Pin(3, machine.Pin.OUT)
+
 while True:
 
     if button_0.value() == 0:
-        print("Button 2")
         onboard_led.toggle()
+        fan0.toggle()
         utime.sleep(0.5)
 
     if button_1.value() == 0:
-        print("Button 2")
         onboard_led.toggle()
+        fan1.toggle()
         utime.sleep(0.5)
